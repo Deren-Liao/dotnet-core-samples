13668f920c46784ab813640fb18477960c43897c
Deren Liao <derenl@google.com>
2017-07-19 23:50:11 +0000

    Console App 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Press X to quit.");
+            while (Console.ReadKey().Key != ConsoleKey.X) { }
+        }
+    }
+}
